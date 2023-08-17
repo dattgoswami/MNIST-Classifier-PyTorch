d59aca2454fdf8df88c5f37f6057586225916590
Datt Goswami <dattgoswami@gmail.com>
2023-08-17 15:23:52 -0600

    refactored the script

--- mnist_pytorch.py
+++ mnist_pytorch.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 
-# Define the model with convolutional layers and dropout
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -28,43 +27,59 @@ class Net(nn.Module):
         x = self.fc2(x)
         return x
 
-# Load the MNIST dataset
-transform = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-train_dataset = datasets.MNIST(
-    './data', train=True, download=True, transform=transform)
-test_dataset = datasets.MNIST('./data', train=False, transform=transform)
-train_loader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=32, shuffle=True)
-test_loader = torch.utils.data.DataLoader(
-    test_dataset, batch_size=1000, shuffle=False)
+def load_data():
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    train_dataset = datasets.MNIST(
+        './data', train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST('./data', train=False, transform=transform)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=32, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=1000, shuffle=False)
+    
+    return train_loader, test_loader
 
-# Initialize the model and define the optimizer and loss function
-model = Net()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-loss_fn = nn.CrossEntropyLoss()
+def train_model(model, train_loader, optimizer, loss_fn, device, epochs=5):
+    model.train()
+    for epoch in range(epochs): 
+        for batch_idx, (data, target) in enumerate(train_loader):
+            optimizer.zero_grad()
+            output = model(data)
+            loss = loss_fn(output, target)
+            loss.backward()
+            optimizer.step()
 
-# Train the model
-for epoch in range(5):  # Number of epochs
-    for batch_idx, (data, target) in enumerate(train_loader):
-        optimizer.zero_grad()
-        output = model(data)
-        loss = loss_fn(output, target)
-        loss.backward()
-        optimizer.step()
 
-# Test the model
-test_loss = 0
-correct = 0
-with torch.no_grad():
-    for data, target in test_loader:
-        output = model(data)
-        test_loss += loss_fn(output, target).item()
-        pred = output.argmax(dim=1, keepdim=True)
-        correct += pred.eq(target.view_as(pred)).sum().item()
+def test_model(model, test_loader, loss_fn, device):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            test_loss += loss_fn(output, target).item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-test_loss /= len(test_loader.dataset)
+    test_loss /= len(test_loader.dataset)
+    accuracy = 100. * correct / len(test_loader.dataset)
+    return test_loss, accuracy
 
-print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-    test_loss, correct, len(test_loader.dataset),
-    100. * correct / len(test_loader.dataset)))
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Load data
+    train_loader, test_loader = load_data()
+
+    # Initialize the model, optimizer, and loss function
+    model = Net().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    loss_fn = nn.CrossEntropyLoss()
+
+    # Train and test the model
+    train_model(model, train_loader, optimizer, loss_fn, device)
+    test_loss, accuracy = test_model(model, test_loader, loss_fn, device)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {:.0f}%\n'.format(
+        test_loss, accuracy))
